b47173f8d41d5d26be7bbf2e21fde67d8a3d7995
Daniil Mikhailov <danicheleos@gmail.com>
2024-05-19 17:14:15 +0200

    build

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'damora-test'`, () => {
+  it(`should have as title 'Da-mora'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('damora-test');
+    expect(app.title).toEqual('Da-mora');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('damora-test app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Da-mora app is running!');
   });
 });
